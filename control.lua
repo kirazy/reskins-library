8f43322ddea61406b66d318605b4f410af14b7e1
Kirazy <35714562+kirazy@users.noreply.github.com>
2020-11-19 02:36:03 -0800

    Update locale contexts, names, add notification locales, mod-name locales

--- control.lua
+++ control.lua
@@ -8,7 +8,7 @@ local function on_configuration_changed(data)
     if data.mod_changes and data.mod_changes["reskins-library"] and data.mod_changes["reskins-library"].old_version then
         -- 1.0.4 update
         if data.mod_changes["reskins-library"].old_version <= "1.0.3" then
-            game.print({"", "[", {"mod-name.reskins-library"}, "] ", {"reskins-updates.reskins-1-0-4-update"}})
+            game.print({"", "[", {"mod-name.reskins-library"}, "] ", {"reskins-updates.reskins-1-0-4-update", {"mod-setting-name.reskins-lib-blend-mode"}}})
         end
     end
 end
