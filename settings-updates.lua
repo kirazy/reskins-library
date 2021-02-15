fccd58bd25d36c602b1631bb59d65d7538567285
Kira <35714562+kirazy@users.noreply.github.com>
2021-02-15 12:06:37 -0800

    Fix concatenation not recognizing localizedstrings at all

--- settings-updates.lua
+++ settings-updates.lua
@@ -4,27 +4,33 @@
 -- See LICENSE.md in the project directory for license information.
 
 -- Concatenate the mod description string with the default description string for boolean settings
-local function concatenate_bool(setting_name, default)
-    if default then
+local function concatenate_bool(setting)
+    if setting.default then
         string_default = "true"
     else
         string_default = "false"
     end
 
-    return {"", {"mod-setting-description."..setting_name}, {"reskins-defaults."..string_default}}
+    -- Check for an already set localised_description
+    if setting.localised_description then
+        return {"", setting.localised_description, {"reskins-defaults."..string_default}}
+    else
+        return {"", {"mod-setting-description."..setting.name}, {"reskins-defaults."..string_default}}
+    end
 end
 
 local valid_prefix = {
     "reskins%-lib",
     "reskins%-bobs",
-    "reskins%-angels"
+    "reskins%-angels",
+    "reskins%-compatibility",
 }
 
 -- Iterate through the boolean settings table and grab all the reskins settings and set the description
 for _, setting in pairs(data.raw["bool-setting"]) do
     for _, prefix in pairs(valid_prefix) do
         if string.find(setting.name, prefix) then
-            setting.localised_description = concatenate_bool(setting.name, setting.default_value)
+            setting.localised_description = concatenate_bool(setting)
             goto continue
         end
     end
