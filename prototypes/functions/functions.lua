d46694bab5708f7e2564161e737aae96211df376
Kira <kirazy@live.com>
2025-03-27 23:52:47 -0700

    refactor: add resiliency and handle indirect explosions, fixes #8

--- prototypes/functions/functions.lua
+++ prototypes/functions/functions.lua
@@ -160,7 +160,7 @@ end
 ---@class CreateExplosionInputs : CreateRemnantInputs
 
 ---Copies the Factorio explosion specified by `inputs.base_entity_name`, extends `data` with a new
----explosion with the name `[name]-explosion`, and assigns it to the named entity
+---explosion with the name `ar-[name]-explosion`, and assigns it to the named entity
 ---@param name string
 ---@param inputs CreateExplosionInputs
 ---```
@@ -170,45 +170,155 @@ end
 --- }
 ---```
 function reskins.lib.create_explosion(name, inputs)
-	local explosion = util.copy(data.raw["explosion"][inputs.base_entity_name .. "-explosion"])
-	explosion.name = name .. "-explosion"
-	data:extend({ explosion })
+	local explosion_copy = util.copy(data.raw["explosion"][inputs.base_entity_name .. "-explosion"])
+	explosion_copy.name = "ar-" .. name .. "-explosion"
+	log("Created explosion " .. explosion_copy.name .. " as setup for entity " .. name .. " copied from " .. inputs.base_entity_name)
+	data:extend({ explosion_copy })
 
 	-- Assign explosion to originating entity
-	data.raw[inputs.type][name]["dying_explosion"] = explosion.name
+	data.raw[inputs.type][name]["dying_explosion"] = explosion_copy.name
 end
 
----Copies the Factorio particle specified by `base_entity_name`, applies tints, extends `data`
----with a new particle with the name `[name]-[base-particle-name]-tinted`, and assigns it to the named explosion
----@param name string #
----@param base_entity_name string # Name of Factorio reference entity to copy from, e.g. `stone-furnace`
----@param base_particle_name string # Key for `reskins.lib.particle_index`
----@param key integer # Index corresponding to the particle within the `explosion` prototype
----@param tint data.Color
-function reskins.lib.create_particle(name, base_entity_name, base_particle_name, key, tint)
-	local particle = util.copy(data.raw["optimized-particle"][base_entity_name .. "-" .. base_particle_name])
-	particle.name = name .. "-" .. base_particle_name .. "-tinted"
-	particle.pictures.sheet.tint = tint
-	data:extend({ particle })
+---Gets the preferred explosion for a given `explosion`, preferring the indirect base explosion, if it exists.
+---@param explosion data.ExplosionPrototype The explosion to check for a base explosion.
+---@return data.ExplosionPrototype
+local function get_preferred_explosion(explosion)
+	-- Several entities have a redirection to a base explosion entity, so we need to check for that and use it
+	-- preferentially.
 
-	-- Assign particle to originating explosion
-	---@type table|nil
-	local target_effects = try_get_value(data.raw.explosion, name .. "-explosion", "created_effect", "action_delivery", "target_effects")
+	---@type data.TriggerEffect[]|nil
+	local target_effects = try_get_value(explosion, "created_effect", "action_delivery", "target_effects")
+
+	if not target_effects or #target_effects == 0 then
+		return explosion
+	end
+
+	if target_effects[1].type == "create-explosion" then
+		-- Copy the explosion reference and update our explosion, if it doesn't already exist, as we may visit the same
+		-- explosion multiple times.
+		local explosion_base_name = target_effects[1].entity_name
+
+		local base_explosion = data.raw["explosion"][explosion_base_name]
+		if base_explosion.name:find("ar-", 1, true) then
+			return base_explosion
+		end
+
+		base_explosion = util.copy(data.raw["explosion"][explosion_base_name])
+		base_explosion.name = "ar-" .. explosion.name .. "-base"
+		data:extend({ base_explosion })
+
+		-- Update the explosion reference name.
+		target_effects[1].entity_name = base_explosion.name
+		return base_explosion
+	end
+
+	return explosion
+end
+
+---A map of particle names to particle exclusions to prevent false positives.
+local particle_exclusions_map = {
+	["metal-particle-medium"] = {
+		"long-metal-particle-medium",
+	},
+}
+
+---Checks if the given `particle_name` is a match for the `base_particle_name`, excepting any exclusions.
+---@param particle_name data.ParticleID The particle name to check.
+---@param base_particle_name string The base particle name to check against.
+---@return boolean # `true` if the particle name is a match for the base particle name, otherwise `false`.
+local function is_particle_name_intended_match(particle_name, base_particle_name)
+	if particle_name:find(base_particle_name, 1, true) then
+		local exclusions = particle_exclusions_map[base_particle_name]
+		if exclusions then
+			for _, exclusion in pairs(exclusions) do
+				if particle_name:find(exclusion, 1, true) then
+					return false
+				end
+			end
+		end
+
+		return true
+	end
+
+	return false
+end
+
+---Gets the preferred particle by checking for a particle name in `particle_effects` that contains the given
+---`base_particle_name`. The method will preferentially select the particle at the index matching `key`, if it exists.
+---Otherwise, the method will select the first particle that matches the `base_particle_name`.
+---@param particle_effects data.CreateParticleTriggerEffectItem[] The particle effects to search through.
+---@param base_particle_name string The base particle name to search for.
+---@param key integer The key to preferentially select, if it exists.
+---@return data.CreateParticleTriggerEffectItem|nil # The preferred particle effect, or `nil` if none are found.
+local function get_preferred_particle(particle_effects, base_particle_name, key)
+	local preferred_particle
+	local fallback_particle
+	for index, effect in pairs(particle_effects) do
+		if is_particle_name_intended_match(effect.particle_name, base_particle_name) then
+			if index == key then
+				preferred_particle = effect
+				break
+			elseif not fallback_particle then
+				fallback_particle = effect
+			end
+		end
+	end
+
+	return preferred_particle or fallback_particle
+end
+
+---Links a given `explosion` to the given `particle` by checking for particle effects in the explosion's target effects
+---that match the `base_particle_name`. The method will preferentially select the particle at the index matching `key`,
+---if it exists. Otherwise, the method will select the first particle that matches the `base_particle_name` to establish
+---the link.
+---@param explosion data.ExplosionPrototype The explosion to link the particle to.
+---@param particle data.ParticlePrototype The particle to link to the explosion.
+---@param base_particle_name string The base particle name to search for.
+---@param key integer The key to preferentially select, if it exists.
+local function link_particle_to_explosion(explosion, particle, base_particle_name, key)
+	local target_effects = try_get_value(explosion, "created_effect", "action_delivery", "target_effects")
 	if not target_effects or #target_effects == 0 then
 		return
 	end
 
-	-- Check if the target_effects table has at least one member, check if that member is of type =
-	-- "create-explosion", and if so, collection the entity name and then go find THAT explosion.
-	if target_effects[1].type == "create_explosion" then
-		local explosion_reference_name = target_effects[1].entity_name
-
-		target_effects = try_get_value(data.raw.explosion, explosion_reference_name, "created_effect", "action_delivery", "target_effects")
+	-- Filter target_effects for all particle effects.
+	local particle_effects = {}
+	for _, effect in pairs(target_effects) do
+		if effect.type == "create-particle" then
+			table.insert(particle_effects, effect)
+		end
 	end
 
-	local target_effect = try_get_value(target_effects, key)
-	if target_effect and target_effect.type == "create_particle" then
-		target_effect.particle_name = particle.name
+	if #particle_effects > 0 then
+		local particle_effect = get_preferred_particle(particle_effects, base_particle_name, key)
+
+		if particle_effect then
+			particle_effect.particle_name = particle.name
+		end
+	end
+end
+
+---Copies the Factorio particle specified by `base_entity_name`, applies tints, extends `data`
+---with a new particle with the name `[name]-[base-particle-name]-tinted`, and links it to the named explosion
+---@param name data.EntityID The name of the entity prototype for which an AR explosion exists.
+---@param base_entity_name data.EntityID Name of Factorio reference entity to copy from, e.g. `stone-furnace`
+---@param base_particle_name string A value from `reskins.lib.particle_index` used to find the particle to copy.
+---@param key integer Index corresponding to the particle within the explosion target_effects table.
+---@param tint data.Color The tint to apply to the particle.
+function reskins.lib.create_particle(name, base_entity_name, base_particle_name, key, tint)
+	local particle = util.copy(data.raw["optimized-particle"][base_entity_name .. "-" .. base_particle_name])
+
+	particle.name = "ar-" .. name .. "-" .. base_particle_name .. "-tinted"
+	particle.pictures.sheet.tint = tint
+
+	data:extend({ particle })
+
+	local explosion = data.raw["explosion"]["ar-" .. name .. "-explosion"]
+	if not explosion then
+		return
+	else
+		local preferred_explosion = get_preferred_explosion(explosion)
+		link_particle_to_explosion(preferred_explosion, particle, base_particle_name, key)
 	end
 end
 
